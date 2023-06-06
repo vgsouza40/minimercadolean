a831fc6aa5602e5458566b58f7a0d0e8b8566fc4
Victor Corrêa <109831967+vgsouza40@users.noreply.github.com>
2023-06-06 20:52:37 -0300

    Update deleteVariationController.php

--- admin/controller/deleteVariationController.php
+++ admin/controller/deleteVariationController.php
@@ -1,6 +1,7 @@
 <?php
 
-    include_once "../config/dbconnect.php";
+    session_start();
+    include("./conexao.php");
     
     $id=$_POST['record'];
     $query="DELETE FROM product_size_variation where variation_id='$id'";
